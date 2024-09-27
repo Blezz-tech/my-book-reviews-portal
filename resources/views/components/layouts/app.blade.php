87a9283b25b8e503bed4e5f6c6c66272e9a46520
blezz-tech <markus.jenya04@yandex.ru>
2024-09-27 13:58:28 +0300

    Добавление layout'а

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="antialiased" lang="ru">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <title>{{ $pageTitle ?? 'Главная страница' }}</title>
+    {{-- <link href="/assets/favicon.ico" rel="shortcut icon" type="image/x-icon"> --}}
+</head>
+<body>
+    <x-panels.navbar />
+    <main>
+        {{ $slot }}
+    </main>
+    <script src="/js/bootstrap.bundle.js"></script>
+</div>
+
+</body>
+</html>
