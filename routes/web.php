8c3a8d95ea8712a709b0628825daf8fd1d263012
blezz-tech <markus.jenya04@yandex.ru>
2024-09-27 13:26:55 +0300

    Создание PagesController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PagesController::class, 'home'])->name('home');
 
 Route::get('/books', [BooksController::class, 'index'])->name('users.books');
