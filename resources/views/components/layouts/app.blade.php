6620261721831bf5bce7e37037d86659a46ff441
blezz-tech <markus.jenya04@yandex.ru>
2024-09-27 14:00:33 +0300

    Исправление layout'а
    добавление выравнивания по контейнеру из bootstrap'а

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,19 +1,24 @@
 <!doctype html>
 <html class="antialiased" lang="ru">
+
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/css/bootstrap.css">
     <title>{{ $pageTitle ?? 'Главная страница' }}</title>
     {{-- <link href="/assets/favicon.ico" rel="shortcut icon" type="image/x-icon"> --}}
 </head>
+
 <body>
     <x-panels.navbar />
-    <main>
-        {{ $slot }}
-    </main>
+    <div class="container">
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
     <script src="/js/bootstrap.bundle.js"></script>
-</div>
+    </div>
 
 </body>
+
 </html>
