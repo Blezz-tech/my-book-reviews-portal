a25f6b7c141d5eaaed6a32a8c9a1b77903a3327a
blezz-tech <markus.jenya04@yandex.ru>
2024-09-27 13:58:49 +0300

    Добавление BooksController

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -11,7 +11,7 @@ class BooksController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.books.index');
     }
 
     /**
---
+++ resources/views/pages/books/index.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app>
+MURMUR
+</x-layouts.app>
