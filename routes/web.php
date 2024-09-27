1c5cca98a9be2024828821a6bcddf707383d2baa
blezz-tech <markus.jenya04@yandex.ru>
2024-09-27 13:26:43 +0300

    Создание BooksController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', [BooksController::class, 'index'])->name('users.books');
